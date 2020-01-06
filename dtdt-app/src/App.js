9299ed307775ffd73c5532adc8fe47d9d82d3859
laurenelle <lauren.elle.t@gmail.com>
2020-01-05 18:10:27 -0800

    add balooie image

--- dtdt-app/src/App.js
+++ dtdt-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import baloo from './majesticBalooie.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={baloo} alt="mascot"/>
         <p>
           Dog Training Data Tool
         </p>
