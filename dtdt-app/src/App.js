865abee63baf080a4defb97b3d1f6576f9e594b1
laurenelle <lauren.elle.t@gmail.com>
2020-01-06 16:57:09 -0800

    add react bootstrap

--- dtdt-app/src/App.js
+++ dtdt-app/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import baloo from './majesticBalooie.png';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+        crossorigin="anonymous"
+      />
       <header className="App-header">
-        <img src={baloo} alt="mascot"/>
         <p>
           Dog Training Data Tool
+        </p>
+        <img src={baloo} alt="mascot"/>
+        <p>
+          <Button>Boop</Button>
         </p>
       </header>
     </div>
